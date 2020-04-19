a3a87c6bf1d09d1cc3190f74622ed791b8344e05
tprice2 <42660616+tprice2@users.noreply.github.com>
2020-04-19 12:26:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/finallab/MainActivity.kt
@@ -0,0 +1,60 @@
+package com.example.android.finallab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    enum class LoginSuccess
+        constructor(val intValue: Int)
+        {
+            login(1),
+            password(2),
+            success(0)
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val txtLogin = findViewById<TextView>(R.id.idLoginText)
+        val txtPassword = findViewById<TextView>(R.id.idPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            when (CheckLogin(txtLogin.text.toString(), txtPassword.text.toString()))
+            {
+                LoginSuccess.login -> {
+                    Toast.makeText(applicationContext, getString(R.string.errMessageLogin), Toast.LENGTH_LONG).show()
+                    txtLogin.requestFocus()
+                }
+
+                LoginSuccess.password -> {
+                    Toast.makeText(applicationContext, getString(R.string.errMessagePassword), Toast.LENGTH_LONG).show()
+                    txtLogin.requestFocus()
+                }
+
+                else ->
+                    Toast.makeText(applicationContext, "Success", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    fun CheckLogin(txtLogin:String, txtPassword: String): LoginSuccess{
+        val holdLogin = "Trevor"
+        val holdPass = "password"
+
+        if(holdLogin != txtLogin)
+        {
+            return LoginSuccess.login
+        }
+        return if (holdPass != txtPassword)
+        {
+            return LoginSuccess.password
+        }
+        else LoginSuccess.success
+    }
+}
